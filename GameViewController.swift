a41729cada102bcec802623bfedf038523b03798
Olivia Mellen <omellen2269@stu.d214.org>
2019-05-01 14:47:27 -0500

    changed view to iPad 10.5 and added second view controller

---
+++ GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  Snake🐍
+//
+//  Created by Olivia Mellen on 5/1/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
