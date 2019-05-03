4879a580d99aa0a81c6c2d1b6b14ebec01234a57
Olivia Mellen <omellen2269@stu.d214.org>
2019-05-03 14:50:30 -0500

    generated random apple

--- Snake🐍/GameViewController.swift
+++ Snake🐍/GameViewController.swift
@@ -23,7 +23,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         create()
-        // Do any additional setup after loading the view.
+        spawnApple()
     }
     func create() {
         let viewsControllerLink = Views(gridAmount: gridSize)
@@ -42,6 +42,9 @@ class GameViewController: UIViewController {
         }
     }
     func spawnApple() {
-        
+        var appleView: Int = Int(arc4random_uniform(UInt32(gridSize*gridSize)))
+        if gridViews[appleView].backgroundColor == UIColor.black {
+            gridViews[appleView].backgroundColor = UIColor.red
+        }
     }
 }
