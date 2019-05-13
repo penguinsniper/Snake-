d9147e58957bd34f209d46241fc7b1b39242e9c7
Ryan Lau <rlau1185@stu.d214.org>
2019-05-13 14:31:10 -0500

    audio

--- Snake🐍/GameViewController.swift
+++ Snake🐍/GameViewController.swift
@@ -113,6 +113,15 @@ var playSound = AVAudioPlayer()
     func death() {
         numIntoTheSnakeArray = snakeArray.count - 1
         timerTwo = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: (#selector(GameViewController.changeColorAtDeath)), userInfo: nil, repeats: true)
+        do {
+            playSound = try AVAudioPlayer(contentsOf: URL.init (fileURLWithPath: Bundle.main.path(forResource: "doorKickDownSoundEffect", ofType: "mp3")!))
+            playSound.prepareToPlay()
+            playSound.play()
+            
+        } catch {
+            print("error, no audio")
+        }
+        
     }
     
     @objc func changeColorAtDeath() {
