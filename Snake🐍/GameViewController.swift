483ef31a1f96ea9461bb0286f2ddd723e480057b
Olivia Mellen <omellen2269@stu.d214.org>
2019-05-13 14:45:39 -0500

    still working on high score

--- Snake🐍/GameViewController.swift
+++ Snake🐍/GameViewController.swift
@@ -39,7 +39,7 @@ var playSound = AVAudioPlayer()
     @IBOutlet var highScoreLabel: UILabel!
     
     var score = 0
-    var highscore = 0
+    var highScore = UserDefaults.standard.value(forKey: "highScore") as! Int
     
     var tickCount = 1
     var time = Timer()
@@ -182,11 +182,9 @@ var playSound = AVAudioPlayer()
                 scoreLabel.text = "Score: \(score)"
                 appleCreate = true
                 touchApple = true
-                if score > highscore {
-                    highscore = score
-                    UserDefaults.standard.set(highscore, forKey: "highscore")
-                    let newHighscore = UserDefaults.standard.integer(forKey: "highscore")
-                    highScoreLabel.text = "High Score: \(newHighscore)"
+                if score > highScore {
+                    UserDefaults.setValue(score, forKey: "highScore")
+                    highScoreLabel.text = "High Score: \(highScore)"
                 }
             }
             if gridViews[snakeGoingToGo].backgroundColor == UIColor.green {
