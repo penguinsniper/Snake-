18d3d0724641781929341ed3d2e7fa107195a862
Ryan Lau <rlau1185@stu.d214.org>
2019-05-14 14:34:47 -0500

    added more colours

--- Snake🐍/ColorViewController.swift
+++ Snake🐍/ColorViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ColorViewController.swift
 //  Snake🐍
-//
+//sdVfwEFwaef
 //  Created by max ortman on 5/9/19.
 //  Copyright © 2019 John Hersey High School. All rights reserved.
 //
@@ -9,13 +9,14 @@
 import UIKit
 
 class ColorViewController: UIViewController {
+   
 
     @IBOutlet var mainColor: UIPickerView!
     @IBOutlet var secondColor: UIPickerView!
     var pickerView: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        pickerView = ["White", "Blue", "Gray","Orange", "Cyan"]
+        pickerView = ["White", "Blue", "Gray","Orange", "Cyan","Magenta","Yellow", "Purple"]
 
         // Do any additional setup after loading the view.
     }
