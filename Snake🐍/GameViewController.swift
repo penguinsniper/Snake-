10f5c786c6658aff71ecd570bfa5eb6dfe98570b
Ryan Lau <rlau1185@stu.d214.org>
2019-05-15 14:45:52 -0500

    aefafe

--- Snake🐍/GameViewController.swift
+++ Snake🐍/GameViewController.swift
@@ -187,15 +187,7 @@ var playSound = AVAudioPlayer()
         var appleCreate = false
         if snakeGoingToGo >= 0 && snakeGoingToGo < gridSize * gridSize - 1 && validSpace == true{
             if gridViews[snakeGoingToGo].backgroundColor == UIColor.red {
-                if touchApple == true{
-                    do {
-                        playSound = try AVAudioPlayer(contentsOf: URL.init (fileURLWithPath: Bundle.main.path(forResource: "appleBiteSoundEffect", ofType: "mp3")!))
-                        playSound.prepareToPlay()
-                        playSound.play()
-                        
-                    } catch {
-                        print("error, no audio")
-                    }
+                
                 }
                 score += 1
                 scoreLabel.text = "Score: \(score)"
