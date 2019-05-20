a789ff06f086a90f61db10fe4d5fa76d319d531a
Olivia Mellen <omellen2269@stu.d214.org>
2019-05-20 15:09:14 -0500

    added fats speed, poison apples, and speed buttons and variables

---
+++ Snake🐍/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  Snake🐍
+//
+//  Created by Olivia Mellen on 5/20/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    var userDefaults = UserDefaults.standard
+    
+    var fastSpeed = false
+    var poisonApples = false
+    var walls = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+    @IBAction func whenFastSpeedPressed(_ sender: Any) {
+        fastSpeed = true
+        userDefaults.set(fastSpeed, forKey: "fastSpeed")
+    }
+    
+    @IBAction func whenPoisonApplesPressed(_ sender: Any) {
+        poisonApples = true
+        userDefaults.set(poisonApples, forKey: "poisonApples")
+    }
+    
+    @IBAction func whenWallsPressed(_ sender: Any) {
+        walls = true
+        userDefaults.set(walls, forKey: "walls")
+    }
+}
