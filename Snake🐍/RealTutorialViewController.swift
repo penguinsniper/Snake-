482bbc11c98df0f9b7545e20da2b6e609b32ea77
Ryan Lau <rlau1185@stu.d214.org>
2019-05-21 15:01:31 -0500

    weg	we4g	24eg	24eg	3w4gqw3hrb

---
+++ Snake🐍/RealTutorialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RealTutorialViewController.swift
+//  Snake🐍
+//
+//  Created by Ryan Lau on 5/21/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class RealTutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
